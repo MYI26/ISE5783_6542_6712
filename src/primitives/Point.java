641930f63c815c01367ca4527c5703821dc03580
Yona Mimoun <yona26.mimoun@gmail.com>
2023-03-21 23:36:02 +0200

    following point

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -21,18 +21,31 @@ public class Point {
         }
     }
 
+    public Point add(Vector vector) {
+        return new Point(
+                xyz.d1 + vector.xyz.d1,
+                xyz.d2 + vector.xyz.d2,
+                xyz.d3 + vector.xyz.d3
+        );}
+
     public Vector substract(Point _p){
         if(_p.equals(this)){
             throw new IllegalArgumentException("unable to create a vector with (0,0,0) as coordinate");
         }
         return new Vector(new Double3(xyz.d1-_p.xyz.d1,xyz.d2-_p.xyz.d2,xyz.d3-_p.xyz.d3));
     }
-    public Point add(Vector vector) {
-        return new Point(
-                xyz.d1 + vector.xyz.d1,
-                xyz.d2 + vector.xyz.d2,
-                xyz.d3 + vector.xyz.d3
-        );
+
+        public Double distanceSquared(Point other) {
+            final Double x1 = xyz.d1;
+            final Double y1 = xyz.d2;
+            final Double z1 = xyz.d3;
+            final Double x2 = other.xyz.d1;
+            final Double y2 = other.xyz.d2;
+            final Double z2 = other.xyz.d3;
+
+            return ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)) + ((z2 - z1) * (z2 - z1));
+        }
+
 
     @Override
     public boolean equals(Object obj) {
