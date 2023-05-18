c5c25f1ade85d8597ef188b0238c3b64c1dab7d0
Aaron <armoun26@icloud.com>
2023-05-18 16:15:29 +0300

    chlav 4 beginnig

---
+++ src/renderer/Camera.java
@@ -0,0 +1,34 @@
+package renderer;
+
+import primitives.Point;
+import primitives.Vector;
+
+public class Camera {
+    private Point location;
+    private Vector vTo,vUp,vRight;
+
+    public Camera(Point _pt, Vector _v1, Vector _v2){
+        double dotProduct=_v1.dotProduct(_v2);
+        if (dotProduct!=0) throw new IllegalArgumentException("The two vector are not perpendicular");
+        location=_pt;
+        vUp=_v1.normalize();
+        vTo=_v2.normalize();
+        vRight=(_v1.crossProduct(_v2)).normalize();
+    }
+
+    public Point getLocation() {
+        return location;
+    }
+
+    public Vector getvRight() {
+        return vRight;
+    }
+
+    public Vector getvTo() {
+        return vTo;
+    }
+
+    public Vector getvUp() {
+        return vUp;
+    }
+}
